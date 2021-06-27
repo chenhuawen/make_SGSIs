131fa17bed7060969b23b5ea006e202f5fa76582
meizu_device <34994815+chenhuawen@users.noreply.github.com>
2021-06-27 16:26:44 +0800

    Add files via upload

---
+++ fix/JOY.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "
+--------------------
+支持的ROM:
+MIUI
+JOYUI
+Flyme
+H2OS
+Color
+smartisan
+ZUI
+--------------------
+"
+
+ ./JOY.sh
+ echo "修复完成"
+ cd ../
+© 2021 GitHub, Inc.
