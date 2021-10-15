cfe674bf105e223465e5e1e7ff4102423af38729
meizu_device <34994815+chenhuawen@users.noreply.github.com>
2021-10-15 11:32:46 +0800

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,11 +5,9 @@ cd $LOCALDIR
 
 sudo apt install python python3 brotli p7zip openjdk-8-jdk unzip zip curl cpio wget unace unrar zip unzip p7zip-full p7zip-rar sharutils uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev liblz4-tool gawk aria2 python3-pip
 sudo apt-get update --fix-missing
-#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-#sudo python get-pip.py
-sudo pip3 install protobuf 
-sudo pip3 install pycrypto 
-sudo pip3 install pycryptodome
+sudo pip3 install protobuf -i https://pypi.tuna.tsinghua.edu.cn/simple/
+sudo pip3 install pycryptodome -i https://pypi.tuna.tsinghua.edu.cn/simple/
+sudo pip3 install pycrypto -i https://pypi.tuna.tsinghua.edu.cn/simple/
 
 python --version 
 python3 --version
